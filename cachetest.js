2733f857f6080c121f0602c74d94053c3936cd0e
Sean <ussa.say@gmail.com>
2017-09-26 12:43:13 -0700

    cache test #3

--- cachetest.js
+++ cachetest.js
@@ -1,3 +1,18 @@
+function ajax(url,callback,error) {
+  var xmlHttp=new XMLHttpRequest();
+  xmlHttp.onreadystatechange=()=>{
+    if (xmlHttp.readyState===4) xmlHttp.status===200?callback(xmlHttp.responseText):error?error(xmlHttp.status):0;
+  };
+  xmlHttp.open("GET",url,true);
+  xmlHttp.send(null);
+}
 window.onload=e=>{
   document.querySelector('#test').innerHTML="The cache works if you see me! If this text is red as well, the cache works!";
+  ajax("cachetest.txt",e=>{
+    var p=document.createElement('p');
+    p.innerHTML=e;
+    document.body.appendChild(p);
+  },e=>{
+    alert("Couldn't use AJAX to load file; "+e);
+  });
 };
