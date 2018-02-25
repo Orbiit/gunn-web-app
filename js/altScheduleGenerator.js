cb11805c53f68110d458fc341c3c3fbcaae9554f
Sean <ussa.say@gmail.com>
2018-02-25 13:16:15 -0800

    fix parsing for Ugwita
    also new psa

--- js/altScheduleGenerator.js
+++ js/altScheduleGenerator.js
@@ -1,26 +1,30 @@
 function toAlternateSchedules(eventItems, EARLIEST_AM_HOUR = 6) {
   let altSchedules = {};
+  function decodeHTMLEntities(string) { // vulnerable to XSS
+    let span = document.createElement("span");
+    span.innerHTML = string;
+    return span.textContent;
+  }
   for (let i = eventItems.length; i--;) {
     if (/(schedule|extended)/i.test(eventItems[i].summary)) {
       if (!eventItems[i].description) continue;
-      let periodItems = eventItems[i].description.split(/\r?\n/).filter(a => a),
+      let periodItems = eventItems[i].description.replace(/<p>(.*?)<\/p>/g,"$1\n").split(/\r?\n/),
       periods = [];
-      for (let i = 0, lastString = ""; i < periodItems.length; i++) {
+      for (let i = 0; i < periodItems.length; i++) {
         let period = periodItems[i],
-        times = /([0-9]+):([0-9]+)-([0-9]+):([0-9]+)/
-          .exec(period.slice(period.lastIndexOf("(") + 1, period.lastIndexOf(")")));
-        if (times === null) {
-          lastString = period + "\n";
-        } else {
-          times = times.slice(1).map(Number);
-          if (times[0] < EARLIEST_AM_HOUR) times[0] += 12;
-          if (times[2] < EARLIEST_AM_HOUR) times[2] += 12;
+        matches = /(.*?)\(?(1?[0-9]):([0-9]{2})-(1?[0-9]):([0-9]{2})\)?/g.exec(period);
+        if (!decodeHTMLEntities(period).trim()) continue;
+        if (matches) {
+          let times = matches.slice(2, 6).map(Number);
+          if (+times[0] < EARLIEST_AM_HOUR) times[0] += 12;
+          if (+times[2] < EARLIEST_AM_HOUR) times[2] += 12;
           periods.push({
-            name: (lastString + period.slice(0, period.lastIndexOf(" ("))).replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;"),
+            name: matches[1].trim().replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;"),
             start: times[0] * 60 + times[1],
             end: times[2] * 60 + times[3]
           });
-          lastString = "";
+        } else {
+          periods[periods.length - 1].name += periodItems[i].trim().replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;");
         }
       }
       altSchedules[(eventItems[i].start.date || eventItems[i].start.dateTime).slice(5, 10)] = periods;
