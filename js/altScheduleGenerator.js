cfd96a11490377f41392f793525dd1c3b42d5439
Sean <ussa.say@gmail.com>
2018-04-09 17:27:10 -0700

    slightly "smarter"? hopefully!

--- js/altScheduleGenerator.js
+++ js/altScheduleGenerator.js
@@ -1,28 +1,37 @@
 function toAlternateSchedules(eventItems, EARLIEST_AM_HOUR = 6) {
   let altSchedules = {};
-  function decodeHTMLEntities(string) { // vulnerable to XSS
-    let span = document.createElement("span");
-    span.innerHTML = string;
-    return span.textContent;
-  }
   for (let i = eventItems.length; i--;) {
     if (/(schedule|extended)/i.test(eventItems[i].summary)) {
       if (!eventItems[i].description) continue;
-      let periodItems = eventItems[i].description.replace(/<p>(.*?)<\/p>/g,"$1\n").split(/\r?\n/),
+      let periodItems = eventItems[i].description.replace(/<p>(.*?)<\/p>/g,"$1\n").replace(/<\/?[a-z]>/gi, "").replace(/&nbsp;/g, " ").replace(/(\).*?),(.*?\()/g, "$1\n$2").split(/\r?\n/),
       periods = [];
       for (let i = 0; i < periodItems.length; i++) {
         let period = periodItems[i],
         matches = /(.*?)\(?(1?[0-9]):([0-9]{2})-(1?[0-9]):([0-9]{2})\)?/g.exec(period);
-        if (!decodeHTMLEntities(period).trim()) continue;
+        if (!period.trim()) continue;
         if (matches) {
           let times = matches.slice(2, 6).map(Number);
-          if (+times[0] < EARLIEST_AM_HOUR) times[0] += 12;
-          if (+times[2] < EARLIEST_AM_HOUR) times[2] += 12;
-          periods.push({
-            name: matches[1].trim().replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;"),
-            start: times[0] * 60 + times[1],
-            end: times[2] * 60 + times[3]
-          });
+          if (times[0] < EARLIEST_AM_HOUR) times[0] += 12;
+          let startTime = times[0] * 60 + times[1],
+              periodName = matches[1].trim().replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;"),
+              foundDuplicate = false,
+              j;
+          for (j = 0; j < periods.length; j++) {
+            if (periods[j].start === startTime) {
+              foundDuplicate = true;
+              break;
+            }
+          }
+          if (foundDuplicate) {
+            periods[j].name += periodName;
+          } else {
+            if (times[2] < EARLIEST_AM_HOUR) times[2] += 12;
+            periods.push({
+              name: periodName,
+              start: startTime,
+              end: times[2] * 60 + times[3]
+            });
+          }
         } else if (periods.length > 0) {
           periods[periods.length - 1].name += periodItems[i].trim().replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;");
         }
