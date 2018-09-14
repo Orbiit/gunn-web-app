033c66db42a709b33f8f5b8efce80fbe3063a1c5
Sean <ussa.say@gmail.com>
2018-09-14 15:28:07 -0700

    I approve of this change

--- lite/sw.js
+++ lite/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ugwita-cache-v11", // change cache name to force update
+const CACHE_NAME = "ugwita-cache-v12", // change cache name to force update
 urlsToCache = [
   "./",
   "index.html",
