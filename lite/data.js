075c8cc1f12e4d57f9a743315f7566d7e7360279
Sean <ussa.say@gmail.com>
2018-09-14 15:17:53 -0700

    add regex

--- lite/data.js
+++ lite/data.js
@@ -48,3 +48,5 @@ const keywords = ["schedule", "extended", "holiday", "no students", "break", "de
 
 const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const legalHashDateRegex = /^#20[0-9]{2}-[01][0-9]-[0-3][0-9]$/;
